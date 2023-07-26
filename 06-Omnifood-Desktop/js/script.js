f846e90ff0ccb82b0bffb1270de4a21046d45bdb
hardik6869 <sorthiyahardik6869@gmail.com>
2023-07-26 15:40:34 +0530

    06-Implement: Smooth Scrolling

--- 06-Omnifood-Desktop/js/script.js
+++ 06-Omnifood-Desktop/js/script.js
@@ -1,11 +1,41 @@
 // Set Current Year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
+
 yearEl.textContent = currentYear;
 
 // Make Mobile Navigation
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
+
 btnNavEl.addEventListener("click", () => {
   headerEl.classList.toggle("nav-open");
+});
+
+// Smooth Scrolling Animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close Mobile Navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
 });
